a84cbb74df19c5d135c7b51020092b7a8b090539
Jeanpierre Rea <70703943+jeanpierrerea@users.noreply.github.com>
2023-08-28 19:42:16 +0000

    Removed sensitive data, regenerated API key

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ CITY = "&q=London"
 AQI_YES = "&aqi=yes"
 
 FINAL_URL = BASE_URL + JSON_TYPE + "key=" + config.API_KEY + CITY + AQI_YES
-# URL = "https://api.weatherapi.com/v1/current.json?key=6f6b01f43b3b4e6fa75175805232808&q=London&aqi=no"
 
 response = requests.get(FINAL_URL, timeout=10)
 print(response.json())
