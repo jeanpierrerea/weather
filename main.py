a985e9232141fede6781263adb7dd705de492667
Jeanpierre Rea <70703943+jeanpierrerea@users.noreply.github.com>
2023-08-28 20:08:23 +0000

    Created a function to request data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ Weather App """
 
-import requests, config
-
+import requests
+import config
 
 BASE_URL = "http://api.weatherapi.com/v1/"
 JSON_TYPE = "current.json?"
@@ -10,13 +10,16 @@ AQI_YES = "&aqi=yes"
 
 FINAL_URL = BASE_URL + JSON_TYPE + "key=" + config.API_KEY + CITY + AQI_YES
 
-response = requests.get(FINAL_URL, timeout=10)
-print(response.json())
 
 def user_input():
     """ Function for User Input"""
     user_input_string = input("Check weather: ")
     return user_input_string
 
+def request_data():
+    """ Function for requesting weather data"""
+    response = requests.get(FINAL_URL, timeout=10)
+    return response.json()
+
 if __name__ == "__main__":
-    print(user_input())
+    print(request_data())
