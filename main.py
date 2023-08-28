3bf12bab29c3ecfee13701ca25cccc4f6fa0dcf8
Jeanpierre Rea <70703943+jeanpierrerea@users.noreply.github.com>
2023-08-28 20:43:23 +0000

    User input gives correct json data

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ JSON_TYPE = "current.json?"
 CITY = "&q=London"
 AQI_YES = "&aqi=yes"
 
-FINAL_URL = BASE_URL + JSON_TYPE + "key=" + config.API_KEY + CITY + AQI_YES
-
+#final_url = BASE_URL + JSON_TYPE + "key=" + "96b89389842e4c14b4e202506232808" + CITY + AQI_YES
 
 def user_input():
     """ Function for User Input"""
-    user_input_string = input("Check weather: ")
+    user_input_string = input("Choose a city: ")
     return user_input_string
 
 def request_data():
     """ Function for requesting weather data"""
-    response = requests.get(FINAL_URL, timeout=10)
+    city = "&q=" + user_input()
+    final_url = BASE_URL + JSON_TYPE + "key=" + "96b89389842e4c14b4e202506232808" + city + AQI_YES
+    response = requests.get(final_url, timeout=10)
     return response.json()
 
 if __name__ == "__main__":
