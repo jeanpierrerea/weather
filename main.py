c37a74f7c3814038c32955754ac506cf9bad8863
Jeanpierre Rea <70703943+jeanpierrerea@users.noreply.github.com>
2023-08-28 20:44:48 +0000

    Took out API key and regenerated it

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def user_input():
 def request_data():
     """ Function for requesting weather data"""
     city = "&q=" + user_input()
-    final_url = BASE_URL + JSON_TYPE + "key=" + "96b89389842e4c14b4e202506232808" + city + AQI_YES
+    final_url = BASE_URL + JSON_TYPE + "key=" + config.API_KEY + city + AQI_YES
     response = requests.get(final_url, timeout=10)
     return response.json()
 
